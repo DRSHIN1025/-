56380b5846cb198b43f0f73b6bbd5aeecd297c13
DRSHIN1025 <drshin1025@gmail.com>
2025-09-25 15:11:58 +0900

    Fix GitHub Pages configuration for image paths

--- OneDrive/바탕 화면/홈페이지/lerevena-homepage/next.config.js
+++ OneDrive/바탕 화면/홈페이지/lerevena-homepage/next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
     unoptimized: true
   },
   basePath: process.env.NODE_ENV === 'production' ? '/lerevena' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/lerevena/' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/lerevena' : '',
 }
 
 module.exports = nextConfig
